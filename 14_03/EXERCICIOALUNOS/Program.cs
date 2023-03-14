70236fdc1df7f4bc500b8f189ad5a69a720e5424
ProjLucasPrates <100208473+ProjLucasPrates@users.noreply.github.com>
2023-03-14 12:42:42 -0300

    Add files via upload

--- 14_03/EXERCICIOALUNOS/Program.cs
+++ 14_03/EXERCICIOALUNOS/Program.cs
@@ -1,13 +1,22 @@
 ﻿namespace EXERCICIOALUNOS;
+using System;
 class Program
 {
     static void Main(string[] args)
     {
         Alunos aluno1 = new Alunos();
-        aluno1.nome = "Lucas Prates";
-        aluno1.nota1 = 4;
-        aluno1.nota2 = 5;
+        Console.WriteLine("Digite seu nome: ");
+        aluno1.nome = Console.ReadLine();
+        Console.WriteLine("O nome digitado foi: " + aluno1.nome);
 
+        //Recebendo um número do tipo double
+        Console.WriteLine("Digite a primeira nota: ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a segunda nota: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
         aluno1.mensagem();
     }
 }
+
+
