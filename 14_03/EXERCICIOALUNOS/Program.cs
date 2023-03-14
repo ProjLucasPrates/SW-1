14eb5aea50898de1d65ab304f7ee26565bcfec23
ProjLucasPrates <100208473+ProjLucasPrates@users.noreply.github.com>
2023-03-14 12:20:08 -0300

    Add files via upload

---
+++ 14_03/EXERCICIOALUNOS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EXERCICIOALUNOS;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Lucas Prates";
+        aluno1.nota1 = 4;
+        aluno1.nota2 = 5;
+
+        aluno1.mensagem();
+    }
+}
