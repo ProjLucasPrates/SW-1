65f3a3f8c600209988cf3a24e66c7d35ba11f469
ProjLucasPrates <100208473+ProjLucasPrates@users.noreply.github.com>
2023-03-28 12:02:18 -0300

    Add files via upload

---
+++ PROJETO_PESSOA/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PROJETO_PESSOA;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("godofredo", 15);
+        //p1.AlteraNome("Lucas");
+        //p1.AlteraSobreNome("Prates");
+        //p1.AlteraIdade(15);
+        p1.MostraDados();
+    }
+}
